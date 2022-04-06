02c871aa913e94e88b043b27aa95fd7e363398c6
MedLaajili <mohamed.laajili@polytechnicien.tn>
2022-04-06 17:31:04 +0100

    testing records

---
+++ test/saving.js
@@ -0,0 +1,9 @@
+const mocha = require ('mocha');
+const Assert = require('assert')
+//describe tests
+describe('saving recors',function(){
+    //create tests
+    it('saves a record to the database',function(){
+        Assert(2+3===5); 
+    });
+})
